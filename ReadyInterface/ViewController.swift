8bd11d259b354f4a319dc8db862a4bf1185643a2
Dhyanesh Chaudhari <iam.chaudhari@gmail.com>
2017-11-15 14:35:27 -0500

    #1 organization

---
+++ ReadyInterface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadyInterface
+//
+//  Created by Dhyanesh Chaudhari on 2017-11-15.
+//  Copyright © 2017 iamchaudhari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
